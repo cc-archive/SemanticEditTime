c91148d4a30cb88376ee9dda2e193c538f591431
Steren <steren@creativecommons.org>
2008-07-14 13:43:14 -0700

    Actual time

--- SET_AddEditTime.php
+++ SET_AddEditTime.php
@@ -2,22 +2,21 @@
 # (C) 2008 Steren Giannini
 # Licensed under the GNU GPLv2 (or later).
 
-    //Last revision may not have been created at the time of the parse.
-    $revision = Revision::newFromTitle( $parser->getTitle() );
-    if($revision)
-    {   
-    //Edit Time
-        $rev_timestamp = $revision->getTimestamp();
-            //$rev_t = getdate();
-            //$rev_timestamp = date('YmdHis',$rev_t[0]);            
+    //Last revision is not created at the time of the parse, so we can't use this
+    //$revision = Revision::newFromTitle( $parser->getTitle() );
+    //if($revision)
+   
+    /*Edit Time*/
+        //$rev_timestamp = $revision->getTimestamp();
+        $rev_t = getdate();
+        $rev_timestamp = date('YmdHis',$rev_t[0]);            
 
-    //Edit User
-        $rev_user_txt = 'User:' . $revision->getUserText();
-            //$rev_user = User::newFromSession();
-            //$rev_user_txt = 'User:' . $rev_user->getName();          
+    /*Edit User*/
+        //$rev_user_txt = 'User:' . $revision->getUserText();
+        $rev_user = User::newFromSession();
+        $rev_user_txt = 'User:' . $rev_user->getName();          
     
     SMWFactbox::addProperty(/*propertyname:*/ 'Revision time' , /*value*/ $rev_timestamp, /*caption*/ false);
     SMWFactbox::addProperty(/*propertyname:*/ 'Revision user' , /*value*/ $rev_user_txt, /*caption*/ false);
-    }
 
 ?>
